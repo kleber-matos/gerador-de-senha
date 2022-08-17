12b18dbb9d11d77e3e7f7de4f0ac5b58fd5aeb32
kleber Matos <klebermatos2019@icloud.com>
2022-08-17 20:19:03 -0300

    commit

---
+++ script.js
@@ -0,0 +1,41 @@
+    //Declaração das variaveis 
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button"); 
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@-"; //caracteres da senha 
+let novaSenha = "";
+
+
+    
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    
+    let pass = "";  //gerador aléatorio
+    for(i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()*n))
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novaSenha);
+}
+
+let copy = document.querySelector("#on")
+function onn() {
+    copy.classList.remove("on")
+}
